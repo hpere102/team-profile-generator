cecd08591576fc6205d45023059e2ce6a04db9ff
danny p <danny@dannys-MacBook-Pro.local>
2021-11-18 17:43:16 -0500

    functions

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 
 class Employee {
@@ -64,18 +66,6 @@ class Company {
         this.team.push(new Intern(name, id, email, school));
     }
 
-    addEmployee(type, name, id, email, data) {
-        switch(type) {
-            case "manager":
-                return this.addManager(name, id, email, data);
-            case "engineer":
-                return this.addEngineer(name, id, email, data);
-            case "intern":
-                return this.addIntern(name, id, email, data);   
-            default:
-                throw Error("Employee type not supported")  ;                    
-        }
-    }
 
     getHtml() {
 
@@ -93,9 +83,176 @@ class Company {
         
         `
     }
-}
+};
 
-const company = new Company("Apple");
+const promptCompany = companyInfo => {
+
+    inquirer.prompt([
+        {
+          type: 'input',
+          name: 'company-name',
+          message: 'What is the company name?',
+          validate: companyNameInput => {
+            if (companyNameInput) {
+              return true;
+            } else {
+              console.log('Please enter a company name.');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'manager',
+          message: 'Please enter the team managers name?',
+          validate: managerInput => {
+            if (managerInput) {
+              return true;
+            } else {
+              console.log('Please enter a name.');
+              return false;
+            }
+          }
+        },
+        {
+            type: 'input',
+            name: 'manager-id',
+            message: 'Provide the team managers ID?',
+            validate: idInput => {
+              if (idInput) {
+                return true;
+              } else {
+                console.log('Please enter a valid ID.');
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter the team managers email address.',
+            validate: emailInput => {
+              if (emailInput) {
+                return true;
+              } else {
+                console.log('Please enter a valid email address.');
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'office',
+            message: 'Please enter the team managers office number.',
+            validate: officeInput => {
+              if (officeInput) {
+                return true;
+              } else {
+                console.log('Please enter an office number.');
+                return false;
+              }
+            }
+          },
+          
+        {
+          type: 'list',
+          name: 'employee-type',
+          message: 'Would you like to add more employees to the company or are you finished building your team?.',
+          choices: ['Add Engineer',  'Add Intern' , 'Im finished'],
+          validate: choices=> {
+              if(choices === 'Add Enginner') {
+                  engineerPrompt();
+              }
+          }
+          
+        },
+       
+      ])
+
+      const engineerPrompt = engineerInfo => {
+
+        inquirer.prompt([
+            {
+              type: 'input',
+              name: 'company-name',
+              message: 'What is the company name?',
+              validate: companyNameInput => {
+                if (companyNameInput) {
+                  return true;
+                } else {
+                  console.log('Please enter a company name.');
+                  return false;
+                }
+              }
+            },
+
+        ])
+    
+
+      .then(answers => {
+
+        const {title, description, installation, usage, contributing, tests, license, username, email} = answers;
+    
+        const template=`# ${title}
+    
+           
+    
+    
+    
+    ## Description
+    ${description}
+            
+    ## Table of Contents
+    * [Description](#Description) <br>
+    * [Table of Contents](#Table-of-Contents) <br>
+    * [Installation](#Installation) <br>
+    * [Usage](#Usage) <br>
+    * [Tests](#Tests) <br>
+    * [Licenses](#Licenses) <br>
+    * [Questions](#Questions) 
+    
+    ## Installation
+    ${installation}
+    
+    ## Usage
+    ${usage}
+    
+    ## Contributing
+    ${contributing}
+    
+    ## Tests
+    ${tests}
+    
+    ### Licenses
+    ![badge](https://img.shields.io/badge/license-${license}-brightgreen) <br>
+    This project was created under the ${license} license.
+    
+    ### Questions
+    Contact me:
+    
+    [Github](https://www.github.com/${username}) <br>
+    [Email](mailto:${email})
+            
+            
+            `
+        
+           
+    fs.writeFile("./README.md", template, err =>
+      err ? console.log(err) : console.log('You are all set! Your README.md file has been created.')
+    );
+      
+      
+    })
+    };
+}
+    
+    
+    promptCompany();
+
+
+
+
+/*const company = new Company("Apple");
 
 company.addManager("Hector Perez", "3425634", "hector@emaol.com", "332");
 
@@ -103,4 +260,4 @@ company.addEngineer("Hector Perez", "3425634", "hector@emaol.com", "332");
 
 company.addIntern("Hector Perez", "3425634", "hector@emaol.com", "332");
 
-console.log(company.getHtml());
+console.log(company.getHtml()); */ 
